73d72bceefa2e102dbe5fcee528b4ba3913786f6
Carly Ho <carlymho@fastmail.com>
2023-06-26 01:12:37 -0500

    fix ref to previouselementsibling

--- cgi-bin/LJ/HTMLControls.pm
+++ cgi-bin/LJ/HTMLControls.pm
@@ -372,7 +372,7 @@ sub html_text {
             <button
                 type=\"button\"
                 class=\"toggle-show-link\"
-                onclick=\"let curstate=this.parentElement.previousSibling.getAttribute('type');this.parentElement.previousSibling.setAttribute('type', curstate == 'password' ? 'text' : 'password');this.textContent=(curstate == 'password' ? 'Hide password?' : 'Show password?');\"
+                onclick=\"let curstate=this.parentElement.previousElementSibling.getAttribute('type');this.parentElement.previousElementSibling.setAttribute('type', curstate == 'password' ? 'text' : 'password');this.textContent=(curstate == 'password' ? 'Hide password?' : 'Show password?');\"
             >
                 Show Password
             </button>
