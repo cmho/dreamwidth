c3153650be7d76cf371096d0256c31817d63cf1a
Carly Ho <carlymho@fastmail.com>
2023-06-27 17:55:14 -0500

    update layout

--- cgi-bin/LJ/S2Theme/petal.pm
+++ cgi-bin/LJ/S2Theme/petal.pm
@@ -4,13 +4,8 @@ use strict;
 
 sub layouts {
     (
-        "1"  => "one-column",
-        "1s" => "one-column-split",
         "2l" => "two-columns-left",
-        "2r" => "two-columns-right",
-        "3"  => "three-columns-sides",
-        "3r" => "three-columns-right",
-        "3l" => "three-columns-left"
+        "2r" => "two-columns-right"
     )
 }
 sub layout_prop { "layout_type" }
